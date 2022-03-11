5a11d6d1fc321cb0cb924c0cd2c106d2639a209d
Prakash7277 <pkpatil.366@gmail.com>
2022-03-11 10:56:56 +0530

    ADDED: Line Comparison Problem For Institute

---
+++ src/com/infogalaxy/linecomparisonproblem/LineComparison.java
@@ -0,0 +1,7 @@
+package com.infogalaxy.linecomparisonproblem;
+
+public class LineComparison {
+    public static void main(String []args){
+        System.out.println(" Welcome To line Comparison Program Made By Prakash");
+    }
+}
