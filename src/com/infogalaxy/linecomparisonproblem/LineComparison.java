3ef9cf9683f2c9ec8ac311e808544c4b36dc404a
Prakash7277 <pkpatil.366@gmail.com>
2022-03-12 22:39:41 +0530

    MODIFIED: Calculate Of Length

--- src/com/infogalaxy/linecomparisonproblem/LineComparison.java
+++ src/com/infogalaxy/linecomparisonproblem/LineComparison.java
@@ -1,7 +1,25 @@
 package com.infogalaxy.linecomparisonproblem;
 
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String []args){
         System.out.println(" Welcome To line Comparison Program Made By Prakash");
+        Scanner sc = new Scanner(System.in);
+        double x1 = 0;
+        double y1 = 0;
+        double x2 = 0;
+        double y2 = 0;
+        double length = 0;
+        System.out.println("Enter The Y1 Co-Ordinates :");
+        x1 = sc.nextInt();
+        System.out.println("Enter The Y2  Co-Ordinates :");
+        y1 = sc.nextInt();
+        System.out.println("Enter The X1 Co-Ordinates :");
+        x2 = sc.nextInt();
+        System.out.println("Enter The X2 Co-Ordinates :");
+        y2 = sc.nextInt();
+        length = Math.sqrt(Math.pow((x1 - x2),2) + Math.pow((y1 - y2),2));
+        System.out.println(("Length of Line is =" + length));
     }
 }
